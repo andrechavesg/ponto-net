fe23e707c774bff0d4a2a1753204ee33e761c226
André Chaves <andrechavesg@gmail.com>
2024-11-02 13:04:41 -0300

    adicionando rm cache

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ git pull origin main
 docker exec -it ponto-net-php-1 bash -c 'php bin/console d:m:migrate --no-interaction --all-or-nothing'
 docker exec -it ponto-net-php-1 bash -c 'php bin/console cache:clear'
 rm -r var/cache
+mkdir var/cache
+chmod 777 -R var/cache
