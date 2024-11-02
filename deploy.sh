dd8528343921515f49dd377c2837d6a7c75705c0
André Chaves <andrechavesg@gmail.com>
2024-11-02 12:56:01 -0300

    adicionando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-git pull origin master
+git pull origin main
 docker exec -it ponto-net-php-1 bash -c 'php bin/console d:m:migrate --no-interaction --all-or-nothing'
 docker exec -it ponto-net-php-1 bash -c 'php bin/console cache:clear'
