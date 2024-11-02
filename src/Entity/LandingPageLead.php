a116b2094b180e066a322c2b9a314a1969369098
André Chaves <andrechavesg@gmail.com>
2024-11-02 12:54:46 -0300

    adicionando created at

--- src/Entity/LandingPageLead.php
+++ src/Entity/LandingPageLead.php
@@ -28,6 +28,11 @@ class LandingPageLead
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $createdAt = null;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
